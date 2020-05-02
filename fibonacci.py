9f697d235a5df82c270be96cead44e95d00bc126
VeenaHarshitha <43546722+VeenaHarshitha@users.noreply.github.com>
2020-05-02 07:35:50 -0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,43 +1,17 @@
-# -*- coding: utf-8 -*-
-#importing the libraries
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import pandas as pd
-
-#importing the dataset
-#dataset = pd.read_csv('Salary_Data.csv')
-#x = dataset.iloc[:, :-1].values
-#y = dataset.iloc[:, 1].values
-
-#splitting the dataset into training set and testset
-#from sklearn.model_selection import train_test_split
-#x_train,x_test,y_train,y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
-
-#feature scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)"""
-
-#fitting simple linear regression to the training set
-#from sklearn.linear_model import LinearRegression
-#regressor = LinearRegression()
-#regressor.fit(x_train, y_train)
-
-#predicting the test set results
-#y_pred = regressor.predict(x_test)
-
-#visulising the trining set results
-#plt.scatter(x_train, y_train, color = 'red')
-#plt.plot(x_train, regressor.predict(x_train), color = 'blue')
-#plt.title('Salary vs Experience (Training set)')
-#plt.xlabel('Years of Experience')
-#plt.ylabel('Salary')
-#plt.show()  #to specify it is end of graph
-
-#plt.scatter(x_test, y_test, color = 'red')
-#plt.plot(x_train, regressor.predict(x_train), color = 'blue')
-#plt.title('Salary vs Experience (Test set)')
-#plt.xlabel('Years of Experience')
-#plt.ylabel('Salary')
-#plt.show() 
+f1, f2 = 0, 1
+cnt = 0
+num = int(input("Enter the number of terms:"))
+# check if the number of terms is valid
+if num <= 0:
+   print("Enter a positive integer")
+elif num == 1:
+   print("Fibonacci sequence upto",num,":")
+   print(f1)
+else:
+   print("Fibonacci sequence upto",num,":")
+   while cnt < num:
+       print(f1)
+       temp = f1 + f2
+       f1 = f2
+       f2 = temp
+       cnt = cnt + 1
